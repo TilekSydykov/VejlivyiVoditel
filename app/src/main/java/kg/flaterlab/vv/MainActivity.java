48bf3c3d622b21922f3853a6c082b77d666e4e23
TilekSydykov <tilek.sydykov@iaau.edu.kg>
2019-05-20 09:03:06 +0600

    v0.0.1

---
+++ app/src/main/java/kg/flaterlab/vv/MainActivity.java
@@ -0,0 +1,47 @@
+package kg.flaterlab.vv;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
---
+++ app/src/main/java/kg/flaterlab/vv/models/User.java
@@ -0,0 +1,15 @@
+package kg.flaterlab.vv.models;
+
+
+public class User {
+
+    /* constructor */
+
+    private String name;
+    private String surname;
+    private String status;
+    private boolean isOnline;
+
+    /* getters and setters */
+}
+
